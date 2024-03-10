29043976baeee98c2ff6e7fcf0e057b0627bdeed
LXDa <2439185947@qq.com>
2024-03-10 22:48:37 +0800

    Site updated: 2024-03-10 22:48:36

--- js/universe.js
+++ js/universe.js
@@ -115,3 +115,16 @@ function share_() {
 function share() {
     debounce(share_, 300);
 }
+//随机访问文章
+// 发现有时会和当前页面重复，加一个判断
+function randomPost() {
+    fetch('/baidusitemap.xml').then(res => res.text()).then(str => (new window.DOMParser()).parseFromString(str, "text/xml")).then(data => {
+        let ls = data.querySelectorAll('url loc');
+        while (true) {
+            let url = ls[Math.floor(Math.random() * ls.length)].innerHTML;
+            if (location.href == url) continue;
+            location.href = url;
+            return;
+        }
+    })
+}
